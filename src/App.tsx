b725132b8dbed367718d8d46ff4de6c10729ae03
broccolipro <119766660+broccolipro@users.noreply.github.com>
2023-05-23 13:52:25 +0000

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import NavBar from "./components/navigation-top-bar/Nav-Bar";
 
 function App() {
     return (
-        <>
-            Hello
-        </>
+        <NavBar></NavBar>
     );
 }
 
